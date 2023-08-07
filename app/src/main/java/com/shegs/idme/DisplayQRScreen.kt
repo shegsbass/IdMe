dcc4835e2d9ce09e77c9c00b4a717285d124a304
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-08-07 14:10:42 +0100

    Convert QR to PNG and share to other platform

--- app/src/main/java/com/shegs/idme/DisplayQRScreen.kt
+++ app/src/main/java/com/shegs/idme/DisplayQRScreen.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,9 +14,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun DisplayQRScreen(qrCodeBitmap: ImageBitmap) {
+
+    val context = LocalContext.current
 
         Image(
         bitmap = qrCodeBitmap,
@@ -25,4 +29,10 @@ fun DisplayQRScreen(qrCodeBitmap: ImageBitmap) {
             .aspectRatio(1f),
         contentScale = ContentScale.Fit
     )
+
+    Button(
+        onClick = { shareQRCode(context, qrCodeBitmap) }
+    ) {
+        Text("Share QR Code")
+    }
 }
